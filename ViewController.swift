c5c65d9bf97bf4e5ebe0399bbdef77245219bade
Shumpei23-yk <89902387+Shumpei23-yk@users.noreply.github.com>
2021-09-17 18:20:58 +0900

    QuizApp1
    初めてのアプリ

---
+++ ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SampleQuiz
+//
+//  Created by Shumpei on 2021/08/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var startButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        startButton.layer.borderWidth = 2
+        startButton.layer.borderColor = UIColor.black.cgColor
+    }
+
+
+}
+
